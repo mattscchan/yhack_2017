5deae0e72b1f60bfb9dfb2c8c9101ec2a33bb7f8
Fan Ma <fan_ma8@hotmail.com>
2017-12-02 09:59:00 -0500

    end of night 1

--- extension/censoring.js
+++ extension/censoring.js
@@ -1,6 +1,58 @@
 $(document).ready(function(){
+    console.log("asdasd");
     highlightRed([" introducing special tracks", "Also gluten-free"]);
+    console.log(getTitle())
 });
+
+
+
+function getTitle(){
+
+    var title = $("title").html();
+    var reputableArticles = [];
+    var reputableURL = [];
+    
+      var parsedQuery = title.split(" ").join("+").replace(/<[^<>]*>/g, "");
+      console.log(parsedQuery);
+    
+      $.ajax({
+        url: "https://api.cognitive.microsoft.com/bing/v7.0/news/search",
+        method: "GET",
+        headers: {
+          "Ocp-Apim-Subscription-Key": "c87d294a9c8e4effb43d6a3d0ef9859b"
+        },
+        data: {
+          "q": parsedQuery,
+          "mkt": "en-us"
+        }
+      }).done(function(data) {
+          console.log(data);
+        data.value.forEach(function(article) {
+            reputableURL.push(article.url);
+        });
+      });
+
+      reputableURL.forEach(function(){
+
+      });
+      var data = {"target":"John Doe",
+                  "cluster":""}
+      
+
+      $.ajax({
+        url: "https://api.cognitive.microsoft.com/bing/v7.0/news/search",
+        method: "POST",
+        dataType : "json",
+        contentType: "application/json; charset=utf-8",
+        data : JSON.stringify(data),
+      }).done(function(data) {
+        
+      });
+
+      
+
+    return title;
+}
 
 function highlightRed(flaggedText) {
     jQuery.each( flaggedText, function( i, val ) {
@@ -8,5 +60,4 @@ function highlightRed(flaggedText) {
         $("body").html(replaced);
       });
     
-    //$( "#highlightFakeNews" ).toggle( "highlight" );   
 }
