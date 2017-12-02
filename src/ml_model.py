3f9074edb951a1b14f41cb457d1c1639b38b060a
Matthew Chan <matthew.chan2@mail.mcgill.ca>
2017-12-02 16:16:53 -0500

    still trying to do encoding things

--- src/ml_model.py
+++ src/ml_model.py
@@ -11,6 +11,43 @@ from gensim.models import KeyedVectors
 
 pathToBinVectors = '../../GoogleNews-vectors-negative300.bin'
 
+class PhraseVector:
+	def __init__(self, phrase):
+		self.vector = self.PhraseToVec(phrase)
+	# <summary> Calculates similarity between two sets of vectors based on the averages of the sets.</summary>
+	# <param>name = "vectorSet" description = "An array of arrays that needs to be condensed into a single array (vector). In this class, used to convert word vecs to phrases."</param>
+	# <param>name = "ignore" description = "The vectors within the set that need to be ignored. If this is an empty list, nothing is ignored. In this class, this would be stop words."</param>
+	# <returns> The condensed single vector that has the same dimensionality as the other vectors within the vecotSet.</returns>
+	def ConvertVectorSetToVecAverageBased(self, vectorSet, ignore = []):
+		if len(ignore) == 0: 
+			return np.mean(vectorSet, axis = 0)
+		else: 
+			return np.dot(np.transpose(vectorSet),ignore)/sum(ignore)
+
+	def PhraseToVec(self, phrase):
+		cachedStopWords = stopwords.words("english")
+		phrase = phrase.lower()
+		wordsInPhrase = [word for word in phrase.split() if word not in cachedStopWords]
+		vectorSet = []
+		for aWord in wordsInPhrase:
+			try:
+				wordVector=model1[aWord]
+				vectorSet.append(wordVector)
+			except:
+				pass
+		return self.ConvertVectorSetToVecAverageBased(vectorSet)
+
+	# <summary> Calculates Cosine similarity between two phrase vectors.</summary>
+	# <param> name = "otherPhraseVec" description = "The other vector relative to which similarity is to be calculated."</param>
+	def CosineSimilarity(self, otherPhraseVec):
+		cosine_similarity = np.dot(self.vector, otherPhraseVec) / (np.linalg.norm(self.vector) * np.linalg.norm(otherPhraseVec))
+		try:
+			if math.isnan(cosine_similarity):
+				cosine_similarity=0
+		except:
+			cosine_similarity=0		
+		return cosine_similarity
+
 class StemTokenizer(object):
      def __init__(self):
          self.wnl = PorterStemmer()
@@ -18,10 +55,10 @@ class StemTokenizer(object):
          return [self.wnl.stem(t) for t in word_tokenize(doc)]
 
 def preprocess(file_list, tf=False, stopwords=False):
-	sent_list = []
-	words_sent = []
-	bigram_sent = []
-	trigram_sent = []
+	all_sent = []
+	all_trigrams = []
+	all_bigrams = []
+	all_words = []
 	gram_probs = Counter()
 	total_features = 0
 
@@ -39,7 +76,11 @@ def preprocess(file_list, tf=False, stopwords=False):
 		for file in file_list:
 
 			# split the files into sentences and score the difference of the sentences
+			bigram_sent = []
+			trigram_sent = []
+			words_sent = []
 			sent_list = sent_tokenize(file)
+			all_sent.append(sent_list)
 
 			for sentence in sent_list:
 				# clean all the strings and standardize
@@ -67,6 +108,10 @@ def preprocess(file_list, tf=False, stopwords=False):
 				gram_probs.update(ngrams_2)
 				gram_probs.update(word_list)
 
+			all_trigrams.append(trigram_sent)
+			all_bigrams.append(bigram_sent)
+			all_words.append(word_list)
+
 	# turn all the counts into probabilities
 	for key in gram_probs.keys():
 		total_features += gram_probs[key]
@@ -74,26 +119,29 @@ def preprocess(file_list, tf=False, stopwords=False):
 	for key in gram_probs.keys():
 		gram_probs[key] /= (float) (total_features)
 
-	return words_sent, bigram_sent, trigram_sent, sent_list, gram_probs
+	return all_words, all_bigrams, all_trigrams, all_sent, gram_probs
 
-def score_sentences(words_sent, bigram_sent, trigram_sent, sent_list, gram_probs):
-	sent_probs = []
+def score_sentences(all_words, all_bigrams, all_trigrams, all_sent, gram_probs):
+	all_probs = []
 
-	for i in range(0, len(sent_list)):
-		total_prob = 0
-		feature_num = 0
-		for j in range(0, len(words_sent[i])):
-			total_prob += gram_probs[words_sent[i][j]]
-			feature_num += 1
-		for j in range(0, len(bigram_sent[i])):
-			total_prob += gram_probs[bigram_sent[i][j]]
-			feature_num += 1
-		for j in range(0, len(trigram_sent[i])):
-			total_prob += gram_probs[trigram_sent[i][j]]
-			feature_num += 1
-		sent_probs.append(total_prob/feature_num)
+	for file in range(0, len(all_sent)):
+		sent_probs = []
+		for i in range(0, len(all_sent[file])):
+			total_prob = 0
+			feature_num = 0
+			for j in range(0, len(words_sent[file][i])):
+				total_prob += gram_probs[words_sent[file][i][j]]
+				feature_num += 1
+			for j in range(0, len(bigram_sent[i])):
+				total_prob += gram_probs[bigram_sent[file][i][j]]
+				feature_num += 1
+			for j in range(0, len(trigram_sent[i])):
+				total_prob += gram_probs[trigram_sent[file][i][j]]
+				feature_num += 1
+			sent_probs.append(total_prob/feature_num)
+		all_probs.append(sent_probs)
 
-	return sent_probs
+	return all_probs
 
 def get_highlight_sentences(sent_list, sent_probs, min_num=2, percent=0.2):
 	sentence_num = (int)(len(sent_list)*0.2)
@@ -109,6 +157,10 @@ def get_highlight_sentences(sent_list, sent_probs, min_num=2, percent=0.2):
 
 	return highlights
 
+def calculate_similarity(target_highlights, cluster_highlights):
+
+	for article in cluster_highlights
+
 def write_json(highlights):
 	obj = {"payload": highlights}
 
@@ -123,19 +175,21 @@ def parse_json(filename):
 	return [json_obj['target']], json_obj['cluster_list']
 
 def main(args):
-	# target_article, cluster_list = parse_json(args.filename) 
-	
-	# words_sent, bigram_sent, trigram_sent, sent_list, gram_probs = preprocess(target_article)
-	# sent_probs = score_sentences(words_sent, bigram_sent, trigram_sent, sent_list, gram_probs)
-	# target_highlights = get_highlight_sentences(sent_list, sent_probs)
-
-	# words_sent, bigram_sent, trigram_sent, sent_list, gram_probs = preprocess(cluster_list)
-	# sent_probs = score_sentences(words_sent, bigram_sent, trigram_sent, sent_list, gram_probs)
-	# cluster_highlights = get_highlight_sentences(sent_list, sent_probs)
-
 	print ("Loading the data file... Please wait...")
 	model1 = KeyedVectors.load_word2vec_format(pathToBinVectors, binary=True)
 	print ("Successfully loaded 3.6 G bin file!")
+
+	target_article, cluster_list = parse_json(args.filename) 
+	
+	words_sent, bigram_sent, trigram_sent, sent_list, gram_probs = preprocess(target_article)
+	sent_probs = score_sentences(words_sent, bigram_sent, trigram_sent, sent_list, gram_probs)
+	target_highlights = get_highlight_sentences(sent_list, sent_probs)
+
+	words_sent, bigram_sent, trigram_sent, sent_list, gram_probs = preprocess(cluster_list)
+	sent_probs = score_sentences(words_sent, bigram_sent, trigram_sent, sent_list, gram_probs)
+	cluster_highlights = get_highlight_sentences(sent_list, sent_probs)
+
+	highlights, high_confidence = calculate_similarity(target_highlights, cluster_highlights)
 
 	write_json(highlights)
 
--- src/parser-1.py
+++ src/parser-1.py
@@ -4,15 +4,18 @@ import json
 
 def main():
 	examples = []
-	with open('/Users/mattchan/Projects/yhack_2017/data/fake_or_real_news.csv', 'r', newline='') as f:
+	with open('/Users/mattchan/Projects/yhack_2017/data/fake_or_real_news.csv', 'r', newline='', encoding='utf-8') as f:
 		csv_parser = csv.reader(f)
 		count = 0
 
 		for row in csv_parser:
+			# if count == 1:
+			# 	print(row)
 			obj = {"example": row[0], "target": {"title": row[1], "body":row[2]}, "label": row[3]}
 			examples.append(obj)
+			count += 1
 
-	with open('/Users/mattchan/Projects/yhack_2017/data/fake_or_real_news.json', 'w') as f2:
+	with open('/Users/mattchan/Projects/yhack_2017/data/fake_or_real_news.json', mode='w+', encoding='utf-8') as f2:
 		json.dump(examples, f2, ensure_ascii=False)
 
 if __name__ == '__main__':
