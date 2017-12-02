c3ebf36bed7731134de77f06f458a51dfa20f213
Kevin <kevin.nam@mail.mcgill.ca>
2017-12-02 09:41:21 -0500

    added tooltip and started backend

--- extension/suggesting.js
+++ extension/suggesting.js
@@ -1,26 +1,25 @@
 console.log('Suggesting.js loaded');
 
+var numOfHoverables = 0;
+
 $(document).ready(function() {
 
   // Get title of article
   var title = $('h1.headline a').html();
   console.log(title);
-
-  getSuggestions(title);
-
-  $(".hoverableSuggestableFakeNews").hover(function() {
-
-  });
+  highlightAndSuggest(title);
 });
 
-var highlightYellow = function(string) {
-  var replaced = $("body").html().replace(string, '<span style="background-color: yellow;" class="hoverableSuggestableFakeNews">' + string + '</span>');
+var highlightYellow = function(string, index) {
+  var replaced = $("body").html().replace(string, '<span style="background-color: yellow;" title="tooltip" class="hoverableSuggestableFakeNews-' + index +'">' + string + '</span>');
   $("body").html(replaced);
 };
 
-var getSuggestions = function(string) {
+var highlightAndSuggest = function(string) {
 
-  highlightYellow(string);
+  var index = numOfHoverables + 1;
+
+  highlightYellow(string, index);
 
   var parsedQuery = string.split(" ").join("+").replace(/<[^<>]*>/g, "");
   console.log(parsedQuery);
@@ -37,9 +36,45 @@ var getSuggestions = function(string) {
     }
   }).done(function(data) {
     console.log(data);
+    $( ".hoverableSuggestableFakeNews-" + index ).tooltip({
+      content: generateListOfLinks(data),
+      show: null, // show immediately
+      open: function(event, ui)
+      {
+        if (typeof(event.originalEvent) === 'undefined')
+        {
+          return false;
+        }
+
+        var $id = $(ui.tooltip).attr('id');
+
+        // close any lingering tooltips
+        $('div.ui-tooltip').not('#' + $id).remove();
+
+        // ajax function to pull in data and add it to the tooltip goes here
+      },
+      close: function(event, ui)
+      {
+        ui.tooltip.hover(function()
+            {
+              $(this).stop(true).fadeTo(400, 1);
+            },
+            function()
+            {
+              $(this).fadeOut('400', function()
+              {
+                $(this).remove();
+              });
+            });
+      }
+    });
   });
 };
 
-var isRelevant = function(title, string) {
-
+var generateListOfLinks = function(data) {
+  var links = "<h3>Relevant articles</h3>";
+  data.value.slice(-5).forEach(function(article) {
+    links = links + '<li><a href="'+ article.url +'"><b>[' + article.provider[0].name + '</b>] ' + article.name + '</a></li>';
+  });
+  return links;
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,60 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var PythonShell = require('python-shell');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/check/', function(req, res, next) {
+  var target = req.body.target;
+  var cluster = req.body.cluster;
+
+  // var parsedQuery = title.split(" ").join("+").replace(/<[^<>]*>/g, "");
+  //
+  // var options = {
+  //   method: "GET",
+  //   url: 'https://api.cognitive.microsoft.com/bing/v7.0/news/search',
+  //   gzip: true,
+  //   headers: {
+  //     "Ocp-Apim-Subscription-Key": "c87d294a9c8e4effb43d6a3d0ef9859b"
+  //   },
+  //   qs: {
+  //     "q": parsedQuery,
+  //     "mkt": "en-us"
+  //   }
+  // };
+  //
+  // request(options, function(error, response, body) {
+  //   if (response.body) {
+  //     response.body.value
+  //   }
+  //
+  // });
+
+  var options = {
+    mode: 'text',
+    pythonPath: 'path/to/python',
+    pythonOptions: ['-u'],
+    scriptPath: 'path/to/my/scripts',
+    args: ['value1', 'value2', 'value3']
+  };
+
+  PythonShell.run('my_script.py', options, function (err, results) {
+    if (err) throw err;
+    // results is an array consisting of messages collected during execution
+    console.log('results: %j', results);
+  });
+
+
+});
+
+router.get('/alive', function(req, res, next) {
+  res.send({
+    status: 'alive'
+  });
 });
 
 module.exports = router;
