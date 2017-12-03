d9890c60a426b09f274b64e55040d6d84fab9382
Matthew Chan <matthew.chan2@mail.mcgill.ca>
2017-12-03 01:50:12 -0500

    adding some logging

--- src/ml_model.py
+++ src/ml_model.py
@@ -192,7 +192,7 @@ def calculate_similarity(target_builder, cluster_builder, target_highlights):
 
 	for i in range(0, len(cluster_builder)):
 		cosine_similarity = CosineSimilarity(vec_scores[i], target_vec)
-
+		print("cosine similarity", cosine_similarity)
 		if cosine_similarity < max_diff:
 			return target_highlights, 1
 		elif cosine_similarity < average_diff:
@@ -202,6 +202,9 @@ def calculate_similarity(target_builder, cluster_builder, target_highlights):
 
 def write_json(highlights, high_confidence):
 	obj = {"payload": highlights, "confidence": high_confidence}
+
+	print("confidence", obj.confidence)
+	print("payload": obj.payload)
 
 	with open("output.json", 'w') as f:
 		line = json.dump(obj, f)
