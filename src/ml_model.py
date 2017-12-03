2a801667e27fc6b2d90fd7a8c8e6f1a250287bea
Matthew Chan <matthew.chan2@mail.mcgill.ca>
2017-12-02 21:46:01 -0500

    killing fan softly

--- src/ml_model.py
+++ src/ml_model.py
@@ -9,7 +9,7 @@ from nltk.corpus import stopwords as StopWords
 import numpy as np
 from gensim.models import KeyedVectors
 
-pathToBinVectors = '/mnt/ram-disk'
+pathToBinVectors = '/mnt/ram-disk/GoogleNews-vectors-negative300.bin'
 
 # print ("Loading the data file... Please wait...")
 model1 = KeyedVectors.load_word2vec_format(pathToBinVectors, binary=True)
