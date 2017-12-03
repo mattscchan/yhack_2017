c20e8e46aabefb71ef60c2b1d1ef885b4f61665a
Kevin <kevin.nam@mail.mcgill.ca>
2017-12-02 20:40:20 -0500

    typo

--- nlp-server/routes/index.js
+++ nlp-server/routes/index.js
@@ -3,24 +3,24 @@ var router = express.Router();
 var request = require('request');
 var PythonShell = require('python-shell');
 var fs = require('fs');
+var pathToMatthew = __dirname + '/../../src/';
+var options = {
+  mode: 'text',
+  pythonOptions: ['-u'],
+  scriptPath: pathToMatthew,
+  args: [pathToMatthew + 'input.json'],
+  pythonPath: '/usr/bin/python3'
+};
+
 var pyshell = new PythonShell('ml_model.py', options);
 
 router.post('/check', function(req, res, next) {
-  var pathToMatthew = __dirname + '/../../src/';
   fs.writeFile(pathToMatthew + 'input.json', JSON.stringify(req.body), 'utf8', function (err) {
     if (err) {
       return console.log(err);
     }
     console.log("The file was saved!");
   });
-
-  var options = {
-    mode: 'text',
-    pythonOptions: ['-u'],
-    scriptPath: pathToMatthew,
-    args: [pathToMatthew + 'input.json'],
-    pythonPath: '/usr/bin/python3'
-  };
 
   // sends a message to the Python script via stdin
   pyshell.send(pathToMatthew + 'input.json');
